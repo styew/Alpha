383c622350b089a3117450ee653bea28acba303b
Stephan <s.candido-schroeter@oth-aw.de>
2026-07-21 16:52:32 +0200

    update new map and fix collision

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,9 +1,14 @@
-extends AnimatedSprite2D
+extends StaticBody2D
 
+@onready var sprite = $AnimatedSprite2D
 
 func _ready():
+	sprite.play("default")
 
-	play("default")
+	var frame = randi_range(
+		0,
+		sprite.sprite_frames.get_frame_count("default") - 1
+	)
 
-	frame = randi_range(0, sprite_frames.get_frame_count("default") - 1)
-	set_frame_progress(randf())
+	sprite.set_frame(frame)
+	sprite.set_frame_progress(randf())
