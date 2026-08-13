d95112c4a9aa9302708aba13a0b3baea18e78a4d
Stephan <stephan.schroter@yahoo.com>
2026-08-13 23:33:28 +0200

    add system of Rounds-Basic

---
+++ scripts/day_nicht_manager.gd
@@ -0,0 +1,46 @@
+extends Node
+
+
+signal phase_changed
+signal round_changed
+
+
+var current_round := 1
+
+var is_day := true
+
+var day_duration := 3.0
+var night_duration := 3.0
+
+var time_remaining := day_duration
+
+
+func _process(delta):
+
+	time_remaining -= delta
+
+	if time_remaining <= 0:
+		change_phase()
+
+
+func change_phase():
+
+	if is_day:
+
+		is_day = false
+		time_remaining = night_duration
+
+		print("NOITE - Round ", current_round)
+
+		phase_changed.emit()
+
+	else:
+
+		is_day = true
+		current_round += 1
+		time_remaining = day_duration
+
+		print("DIA - Round ", current_round)
+
+		phase_changed.emit()
+		round_changed.emit()
